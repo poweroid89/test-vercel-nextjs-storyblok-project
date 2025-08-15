1db4e813d9087a2a41b17f5e0e72092acf640987
Roman <poweroid89@gmail.com>
2025-08-15 15:32:05 +0300

    test commit 33

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        domains: ['bri.co.id'], // дозвіл на завантаження картинок з bri.co.id
+    },
 };
 
 module.exports = nextConfig;
